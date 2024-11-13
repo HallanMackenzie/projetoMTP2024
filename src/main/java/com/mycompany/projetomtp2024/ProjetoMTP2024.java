a05475b0b8ee45460960a041b1e81c339f588aba
HallanMackenzie <45502280+HallanMackenzie@users.noreply.github.com>
2024-11-13 10:02:30 -0300

    commit

---
+++ src/main/java/com/mycompany/projetomtp2024/ProjetoMTP2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetomtp2024;
+
+/**
+ *
+ * @author hallan
+ */
+public class ProjetoMTP2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
