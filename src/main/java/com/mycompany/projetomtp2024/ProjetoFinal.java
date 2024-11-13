37f167cec7f1bf44f6b4bc19a31fec2fa2505d62
HallanMackenzie <45502280+HallanMackenzie@users.noreply.github.com>
2024-11-13 11:53:44 -0300

    Commit

--- src/main/java/com/mycompany/projetomtp2024/ProjetoFinal.java
+++ src/main/java/com/mycompany/projetomtp2024/ProjetoFinal.java
@@ -1,16 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.projetomtp2024;
 
 /**
  *
  * @author hallan
  */
-public class ProjetoMTP2024 {
-
+public class ProjetoFinal {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LoginScreen tela = new LoginScreen();
+        tela.setVisible(true);
     }
 }
